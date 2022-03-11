280372a05b7ff65e99b7b4bcd8d94f77b2500f04
Les Aker <me@lesaker.org>
2022-03-10 23:54:03 -0500

    add users param

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 # @summary Configure openssh server
 #
+# @param users defines user/key mappings
 class openssh (
+  Hash[String, Array[String]] $users = []
 ) {
   case $facts['os']['family'] {
     'Archlinux': { include openssh::systemd }
