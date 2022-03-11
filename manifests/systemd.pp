d829653acb6c05b69406c38b77de798258c8492a
Les Aker <me@lesaker.org>
2022-03-10 23:51:28 -0500

    fix linting

--- manifests/systemd.pp
+++ manifests/systemd.pp
@@ -4,14 +4,14 @@ class openssh::systemd {
   package { 'openssh': }
 
   -> file { '/etc/ssh/sshd_config':
-    ensure => present,
-    source => 'puppet:///modules/openssh/sshd_config'
+    ensure => file,
+    source => 'puppet:///modules/openssh/sshd_config',
   }
 
   ~> service { 'sshd':
     ensure   => running,
     enable   => true,
-    provider => 'systemd'
+    provider => 'systemd',
   }
 
   group { 'sshaccess': }
@@ -20,24 +20,23 @@ class openssh::systemd {
     ensure => directory,
     owner  => root,
     group  => sshaccess,
-    mode   => '0755'
+    mode   => '0755',
   }
 
   $openssh::users.each |String $user, Array[String] $keys| {
     $homedir = $user ? {
       'root'  => '/root',
-      default => "/home/${user}"
+      default => "/home/${user}",
     }
     file { "/etc/ssh/authorized_keys/${user}":
-      ensure  => present,
-      content => template('openssh/authorized_keys.erb')
+      ensure  => file,
+      content => template('openssh/authorized_keys.erb'),
     }
     user { $user:
       ensure     => present,
       groups     => ['sshaccess'],
       home       => $homedir,
-      managehome => true
+      managehome => true,
     }
   }
-
 }
